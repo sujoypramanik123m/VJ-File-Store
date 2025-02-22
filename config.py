ab514a0eb852741ea80876791607f02c9a328e7d
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-02-22 12:20:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "27589191"))
 API_HASH = environ.get("API_HASH", "094c70fee92b47679abeeb0922e12440")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://ibb.co/5X5VxXM9')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8181241262').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "ToppersFilesStore_Bot") # without @
 PORT = environ.get("PORT", "8080")
@@ -44,6 +44,8 @@ AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or Fal
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+
+PROTECT_CONTENT = bool(environ.get('PROTECT_CONTENT', True))
 
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002440042338"))
