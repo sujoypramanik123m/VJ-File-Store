339c953d46f9bf54c6b6526d17e51fb9b75ba924
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-02-22 14:19:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -66,7 +66,7 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://supertoppersteam.blogspot.com/2025/02/supertoppers.html") # For More Information Check Video On Yt - @Tech_VJ
