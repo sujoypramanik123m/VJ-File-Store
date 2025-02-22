002aaa22951166c63893d97f2f287122c33e1509
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-02-23 00:10:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -65,9 +65,7 @@ def formate_file_name(file_name):
 
 
 @Client.on_message(filters.command("start") & filters.incoming)
-async def start(bot, update):
-    client = bot
-    message = update
+async def start(client, message):
     if AUTH_CHANNEL:
         try:
             btn = await is_subscribed(client, message, AUTH_CHANNEL)
